d0a746e56b245ebd5a4204f4b3524a362f7cfc6a
Axizeaus <axizeaus@gmail.com>
2022-03-26 01:25:42 +0630

    js exercises of TOP

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = Number(n);
+    if (n < 0){
+        return "OOPS";
+    }
+    let arr = fib(n + 1);
+    return arr[arr.length - 2];
 };
 
+function fib(n) {
+    if (n == 0) return [0]
+    if (n == 1) return [0, 1]
+    const arr = fib(n - 1)
+    return [...arr, arr[n-1] + arr[n-2]]
+}
+
+fibonacci(4);
+console.log(fib(10));
+console.log(fibonacci(4));
 // Do not edit below this line
 module.exports = fibonacci;
